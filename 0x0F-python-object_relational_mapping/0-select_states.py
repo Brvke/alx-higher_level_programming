b44d921b53fd93f40335d9b25034113a5f9319bb
Your Name <your.email@example.com>
2024-04-15 02:28:14 +0300

    check

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__" and len(argv) == 4:
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
