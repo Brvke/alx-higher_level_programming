777bd94ff9e1dfee743dad4366a32ce2bf24000e
Your Name <your.email@example.com>
2024-04-15 04:15:58 +0300

    changes

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" first sqldb trial """
+""" Python script for connecting to a database and executing a
+fetch query """
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__" and len(argv) == 4:
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY id ASC" )
     rows = cur.fetchall()
     for row in rows:
         print("{}".format(row))
